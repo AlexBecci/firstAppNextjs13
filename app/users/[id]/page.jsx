ec3beb454fd562295b0d6ed4d9c9a15a9f2c94fd
Alex <beccialex2002@gmail.com>
2023-04-24 19:15:12 -0300

    user Details

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
-function UsersPage() {
-  return (
-    <div><h1>User Details</h1></div>
-  )
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+  return data.data;
 }
 
-export default UsersPage
+async function UsersPage({ params }) {
+  const user = await getUser(params.id);
+  return (
+    <div>
+      <h1>User Details</h1>
+      <div>
+        <img src={user.avatar} alt={user.email} />
+        <h3>
+          {user.id} {user.first_name} {user.last_name}
+        </h3>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default UsersPage;
