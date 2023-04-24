a626ae7a73300c3f8a5aedeb86f89a772718e878
Alex <beccialex2002@gmail.com>
2023-04-24 18:39:21 -0300

    Link from next

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex atque harum magni corporis, facere totam minus quo neque eveniet, voluptas, sint iste ut eaque sapiente libero. Officia aspernatur explicabo quibusdam.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos totam
+        aperiam quidem quibusdam quasi culpa dolorum adipisci dolores tenetur
+        necessitatibus, beatae officiis. Vero maiores qui cum iusto mollitia
+        facilis deleniti. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. In reprehenderit similique omnis nulla debitis itaque id harum
+        nobis voluptatibus. Vitae numquam itaque iste obcaecati, et repudiandae?
+        Culpa non earum soluta.
+      </p>
+    </div>
+  );
+}
+
+export default ServicePage;
