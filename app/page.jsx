a8e2bedb084a46c230934b7d603f51f0f36fda06
Alex <beccialex2002@gmail.com>
2023-04-24 18:54:22 -0300

    Users Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,18 @@
 import React from "react";
+import Users from "../components/Users";
 
 async function fetchUsers() {
   const res = await fetch("http://reqres.in/api/users");
   const data = await res.json();
-  return data.data
+  return data.data;
 }
 async function IndexPage() {
   const users = await fetchUsers();
   return (
     <div>
       <h1>Index Page</h1>
-      <div>
-        {JSON.stringify(users)}
-      </div>
+
+      <Users users={users} />
     </div>
   );
 }
