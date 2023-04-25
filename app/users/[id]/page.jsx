e1991157c67d770cb8947254d22c126f3e0f679f
Alex <beccialex2002@gmail.com>
2023-04-24 21:28:27 -0300

    users id tailwind added

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -7,14 +7,17 @@ async function getUser(id) {
 async function UsersPage({ params }) {
   const user = await getUser(params.id);
   return (
-    <div>
-      <h1>User Details</h1>
-      <div>
-        <img src={user.avatar} alt={user.email} />
-        <h3>
-          {user.id} {user.first_name} {user.last_name}
-        </h3>
-        <p>{user.email}</p>
+    <div className="text-center flex flex-wrap text-black">
+      <div className=" mx-auto my-5 border-2 shadow-sm rounded-lg">
+        <div className=" px-auto md:px-60 bg-gray-100">
+          <img className="mx-auto py-2" src={user.avatar} alt={user.email} />
+        </div>
+        <div className="bg-zinc-50  py-5">
+          <h3 className="text-lg font-bold py-1">
+            {user.id} {user.first_name} {user.last_name}
+          </h3>
+          <p className="text-xs font-medium">{user.email}</p>
+        </div>
       </div>
     </div>
   );
